68db84c828193033ed8dc174b11fc55788b3dc53
Jerry Miller <millerjerrykamsi999@gmail.com>
2026-06-17 10:01:22 +0100

    Fix typo in nested admin app name

--- joy/settings.py
+++ joy/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nested-admin',
+    'nested_admin',
     'cloudinary_storage',
     'cloudinary',
 
